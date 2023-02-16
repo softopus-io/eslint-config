967ee1a6c4fe08304ffd0853079a5c6eba5ed8c8
Matúš Giertl <matus.giertl@icloud.com>
2023-02-16 18:21:47 +0100

    No console and print width change

--- index.js
+++ index.js
@@ -9,28 +9,32 @@ module.exports = {
     ecmaVersion: 11,
     sourceType: 'module',
   },
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   rules: {
     '@typescript-eslint/no-empty-function': 0,
     '@typescript-eslint/no-var-requires': 1,
     '@typescript-eslint/ban-types': [1, { types: { object: false } }],
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
-    "no-unused-vars": "off",
-    "unused-imports/no-unused-imports": "error",
-    "unused-imports/no-unused-vars": [
-      "warn",
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'no-console': 'error',
+    'no-unused-vars': 'off',
+    'unused-imports/no-unused-imports': 'error',
+    'unused-imports/no-unused-vars': [
+      'warn',
       {
-        "vars": "all",
-        "varsIgnorePattern": "^_",
-        "args": "after-used",
-        "argsIgnorePattern": "^_"
-      }
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
     ],
     'prettier/prettier': [
       'error',
       {
-        printWidth: 180,
+        printWidth: 80,
         trailingComma: 'all',
         singleQuote: true,
         endOfLine: 'lf',
