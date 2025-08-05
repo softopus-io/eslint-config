7a1ded96cb79dd342824e61a3dd344ce92ef5aa9
kasikfrantisek <kasik.frantisek97@gmail.com>
2025-08-05 08:32:08 +0200

    feat: flat config update

--- index.js
+++ index.js
@@ -1,99 +1,108 @@
-module.exports = {
-  parser: "@typescript-eslint/parser",
-  env: {
-    es2020: true,
-    node: true,
-    browser: true,
+const js = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const prettier = require("eslint-config-prettier");
+const prettierPlugin = require("eslint-plugin-prettier");
+const simpleImportSort = require("eslint-plugin-simple-import-sort");
+const unusedImports = require("eslint-plugin-unused-imports");
+const unicorn = require("eslint-plugin-unicorn");
+
+module.exports = [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 11,
+        sourceType: "module",
+      },
+    },
+    environment: {
+      browser: true,
+      node: true,
+      es2020: true,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+      "simple-import-sort": simpleImportSort,
+      "unused-imports": unusedImports,
+      unicorn: unicorn,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/no-var-requires": "warn",
+      "@typescript-eslint/no-empty-object-type": "error",
+      "@typescript-eslint/no-unsafe-function-type": "error",
+      "@typescript-eslint/no-wrapper-object-types": "error",
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            ["^next"],
+            ["^\\u0000"],
+            ["^node:"],
+            ["^@?\\w"],
+            ["^@/"],
+            ["^~"],
+            ["^#"],
+            ["^\\.\\./\\.\\./"],
+            ["^\\.\\./"],
+            ["^\\./"],
+          ],
+        },
+      ],
+      "simple-import-sort/exports": "error",
+      "no-console": "error",
+      "no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "unicorn/filename-case": [
+        "error",
+        {
+          case: "snakeCase",
+          ignore: [
+            "^index\\.(js|ts|jsx|tsx)$",
+            "\\.test\\.(js|ts|jsx|tsx)$",
+            "\\.spec\\.(js|ts|jsx|tsx)$",
+            "\\.stories\\.(js|ts|jsx|tsx)$",
+          ],
+        },
+      ],
+      "no-restricted-imports": [
+        "error",
+        {
+          patterns: [
+            {
+              group: ["../../*"],
+              message:
+                "Use path aliases instead of deep relative imports. Consider using @/ or ~/ aliases.",
+            },
+          ],
+        },
+      ],
+      "prettier/prettier": [
+        "error",
+        {
+          printWidth: 80,
+          trailingComma: "all",
+          singleQuote: true,
+          endOfLine: "lf",
+          semi: true,
+          tabWidth: 2,
+        },
+      ],
+    },
   },
-  plugins: [
-    "@typescript-eslint",
-    "simple-import-sort",
-    "unused-imports",
-    "unicorn",
-  ],
-  parserOptions: {
-    ecmaVersion: 11,
-    sourceType: "module",
-  },
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
-  rules: {
-    "@typescript-eslint/no-empty-function": 0,
-    "@typescript-eslint/no-var-requires": 1,
-    "@typescript-eslint/no-empty-object-type": "error",
-    "@typescript-eslint/no-unsafe-function-type": "error",
-    "@typescript-eslint/no-wrapper-object-types": "error",
-    "simple-import-sort/imports": [
-      "error",
-      {
-        groups: [
-          ["^\\u0000"],
-
-          ["^next"],
-
-          ["^node:"],
-
-          ["^@?\\w"],
-
-          ["^@/"],
-          ["^~"],
-          ["^#"],
-
-          ["^\\.\\./\\.\\./"],
-          ["^\\.\\./"],
-          ["^\\./"],
-        ],
-      },
-    ],
-    "simple-import-sort/exports": "error",
-    "no-console": "error",
-    "no-unused-vars": "off",
-    "unused-imports/no-unused-imports": "error",
-    "unused-imports/no-unused-vars": [
-      "warn",
-      {
-        vars: "all",
-        varsIgnorePattern: "^_",
-        args: "after-used",
-        argsIgnorePattern: "^_",
-      },
-    ],
-    "unicorn/filename-case": [
-      "error",
-      {
-        case: "snakeCase",
-        ignore: [
-          "^index\\.(js|ts|jsx|tsx)$",
-          "\\.test\\.(js|ts|jsx|tsx)$",
-          "\\.spec\\.(js|ts|jsx|tsx)$",
-          "\\.stories\\.(js|ts|jsx|tsx)$",
-        ],
-      },
-    ],
-    "no-restricted-imports": [
-      "error",
-      {
-        patterns: [
-          {
-            group: ["../../*"],
-            message:
-              "Use path aliases instead of deep relative imports. Consider using @/ or ~/ aliases.",
-          },
-        ],
-      },
-    ],
-    "prettier/prettier": [
-      "error",
-      {
-        printWidth: 80,
-        trailingComma: "all",
-        singleQuote: true,
-        endOfLine: "lf",
-        semi: true,
-        tabWidth: 2,
-      },
-    ],
-  },
-};
+  prettier,
+];
